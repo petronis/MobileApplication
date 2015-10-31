92b073d9c6d36a989f9fa9ae534811a3c7f49e37
Lukas <lukas.karmanovas@mif.stud.vu.lt>
2015-10-31 17:58:19 +0200

    First page

---
+++ app/src/main/java/com/example/lukas/mobileapplication/study_time.java
@@ -0,0 +1,18 @@
+package com.example.lukas.mobileapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class study_time extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.study_time_fragment, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
